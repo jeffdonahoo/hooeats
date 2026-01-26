1afafe8414ba337ea4798ab8c9029ae97a88acc8
Jane <jane@jane.com>
2026-01-26 13:38:57 -0600

    text

--- src/main/java/com/hooeats/api/HooEatsApplication.java
+++ src/main/java/com/hooeats/api/HooEatsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HooEatsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HooEatsApplication.class, args);
     }
